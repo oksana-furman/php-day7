57a12be588aaa679c7bf2d49a62d8db28baf7e09
Oksana Furman <oksan9a.furman@gmail.com>
2023-03-29 13:17:40 +0200

    update files

--- components/db_connect.php
+++ components/db_connect.php
@@ -1,2 +1,15 @@
 <?php
 
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'restaurant');
+
+$connect = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+$sql = "SELECT * FROM dishes LIMIT 3";
+$resault = $connect->query($sql);
+$rows = $resault->fetch_all(MYSQLI_ASSOC);
+echo "<pre>";
+echo var_dump($rows);
+echo "</pre>";
--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
+    class Vehicles{
+        public $name; 
+        public $brand; 
+        public $model; 
+        public $color = "red"; 
+        public $fuelType = "diesel";
 
+        function __construct($name, $brand, $model){
+            $this->name = $name;
+            $this->brand = $brand;
+            $this->model = $model; 
+        }
+        public function printSmth(){
+            return "This {$this->name}'s brand is {$this->brand} and model is {$this->model}.";
+        }
+    }
+    $car = new Vehicles("car", "Ford", "Focus SW CRDi");
+    $bus = new vehicles("bus", "Volvo", "B8R");
+    $motorcycle = new vehicles("motorcycle", "Yamaha", "V-Max");
+
+    class Ships extends Vehicles{
+        public $makeYear; 
+        public $type; 
+
+        function __construct($name, $type, $makeYear){
+            $this->name = $name;
+            $this->type = $type; 
+            $this->makeYear = $makeYear; 
+        }
+        public function shipAge(){
+            $currentYear = date("Y");
+            return $currentYear - $this->makeYear;
+        }
+        public function shipPrint(){
+            return "This ship' name is {$this->name}, it is an {$this->type} and is {$this->shipAge()} years old.";
+        }
+
+    }
+    $ship = new ships("Argus", "aircraft carrier", 1918);
 
 ?>
 <!DOCTYPE html>
@@ -12,6 +50,17 @@
     <title>PHP Day 7</title>
 </head>
 <body>
+    <div class="container">
+    <div class="m-3 p-3 text-center">
+        <p><?= $car->printSmth(); ?></p>
+        <p><?= $bus->printSmth(); ?></p>
+        <p><?= $motorcycle->printSmth(); ?></p>
+        <p><?= $ship->shipPrint(); ?></p>
+
+        <p>All vehicles in the class "Vehicles" have <span class="text-danger"><?= $car->color ?></span> color and use <?= $car->fuelType ?>.</p>
+    </div>
+    
+    </div>
     
 </body>
 </html>
